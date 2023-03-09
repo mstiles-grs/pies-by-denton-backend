c157fadf30bd3158ff7ec68cce093dd74927e1b8
mstiles-grs <mstiles@grs-usa.com>
2023-03-09 08:14:58 -0600

    before I can work on functions for user specific things I need to make the middleware

--- main.go
+++ main.go
@@ -5,16 +5,42 @@ import (
     "os"
     "io"
     "net/http"
-    "io/ioutil"
     "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
     "github.com/mstiles-grs/pies-by-denton-backend/controllers"
     "github.com/mstiles-grs/pies-by-denton-backend/database"
+	"github.com/mstiles-grs/pies-by-denton-backend/middleware"
 	"github.com/mstiles-grs/pies-by-denton-backend/models"
 	"bytes"
     "encoding/json"
 
 )
+
+func AuthMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        // Retrieve the session ID cookie
+        cookie, err := c.Cookie("pga4_session")
+        if err != nil {
+            log.Println("Error retrieving session ID cookie:", err)
+            c.AbortWithStatus(http.StatusUnauthorized)
+            return
+        }
+
+        // Retrieve the user associated with the session ID
+        session, err := database.Store.Get(cookie)
+        if err != nil {
+            log.Println("Error retrieving session:", err)
+            c.AbortWithStatus(http.StatusUnauthorized)
+            return
+        }
+
+        // Attach the user to the context
+        c.Set("user", session.User)
+
+        // Continue processing the request
+        c.Next()
+    }
+}
 
 func main() {
 
@@ -46,14 +72,14 @@ func main() {
 
 	router.POST("/create/user", func(c *gin.Context) {
 		// Read the request body
-		body, err := ioutil.ReadAll(c.Request.Body)
+		body, err := io.ReadAll(c.Request.Body)
 		if err != nil {
 			log.Println(err)
 			c.AbortWithStatus(http.StatusBadRequest)
 			return
 		}
 		// Restore the request body for downstream middleware/handlers
-		c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+		c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
 
 		// Log the incoming data
 		log.Printf("Request: %s\n", string(body))
@@ -72,14 +98,14 @@ func main() {
 
 	router.POST("/login/user", func(c *gin.Context) {
         // Read the request body
-        body, err := ioutil.ReadAll(c.Request.Body)
+        body, err := io.ReadAll(c.Request.Body)
         if err != nil {
             log.Println(err)
             c.AbortWithStatus(http.StatusBadRequest)
             return
         }
         // Restore the request body for downstream middleware/handlers
-        c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+        c.Request.Body = io.NopCloser(bytes.NewBuffer(body))
 
         // Log the incoming data
         log.Printf("Request: %s\n", string(body))
@@ -95,6 +121,12 @@ func main() {
         // Call the controller function
         userController.LoginUser(c)
     })
+
+    if err := router.Run(":8080"); err != nil {
+        log.Fatalf("Failed to start server: %v", err)
+    }
+
+
 
     if err := router.Run(":8080"); err != nil {
         log.Fatalf("Failed to start server: %v", err)
